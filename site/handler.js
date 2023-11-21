3100c806a7e6531bbf29d83fee79de02443081df
='chrysanthus <='chrysanthus48akash@gmail.com>
2023-11-21 18:54:02 +0530

    . .

---
+++ site/handler.js
@@ -0,0 +1,40 @@
+const leftPaneElements = document.querySelectorAll('.left-pane');
+const rightPane = document.getElementsByClassName('.right-pane');
+
+for (const element of leftPaneElements) {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    // Hide the right pane content before loading new content
+    rightPane.innerHTML = '';
+
+    // Load content based on the clicked element
+    const clickedContent = loadContent(event.target.textContent);
+    rightPane.innerHTML = clickedContent;
+
+    // Show the right pane after loading content
+    rightPane.style.display = 'block';
+  });
+}
+
+// Add a click event listener to hide the right pane on clicks outside the left pane elements
+document.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('left-pane')) {
+    rightPane.style.display = 'none';
+  }
+});
+
+// Function to load content based on the clicked element text
+function loadContent(clickedText) {
+  // Replace this with your actual content-loading logic
+  switch (clickedText) {
+    case 'Link 1':
+      return '<h1>This is the content for Link 1</h1>';
+    case 'Link 2':
+      return '<h1>This is the content for Link 2</h1>';
+    case 'Link 3':
+      return '<h1>This is the content for Link 3</h1>';
+    default:
+      return '';
+  }
+}
